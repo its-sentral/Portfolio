90ae734b192981347d28d91c091d2c6d404286f5
its-sentral <Chavit.sar20@gmail.com>
2025-12-27 14:30:32 +0700

    More information and design in home section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './nav-bar/nav-bar';
+import { BgItem } from './bg-item/bg-item';
+import { FgItem } from './fg-item/fg-item';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet
-    , NavBar
+    , NavBar, BgItem, FgItem
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('test');
+  screenWidth: number = window.innerWidth;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+  }
 }
---
+++ src/app/bg-item/bg-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bg-item',
+  imports: [],
+  templateUrl: './bg-item.html',
+  styleUrl: './bg-item.scss',
+})
+export class BgItem {
+
+}
